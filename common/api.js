82660823b62031d2c45d770b05dd4cde0a46dea3
YS739 <suk921@gmail.com>
2023-01-05 16:38:23 +0900

    Edit api.js destructuring

--- common/api.js
+++ common/api.js
@@ -1,24 +1,28 @@
 const BASE_URL = "https://api.themoviedb.org/3/movie";
 const API_KEY = "cac58e014429ea0819e52ec164529d1c";
 
+// Movie.jsx
 export const getNowPlayings = () =>
   fetch(
     `${BASE_URL}/now_playing?api_key=${API_KEY}&language=en-US&page=1`
   ).then((res) => res.json());
 
-export const getTopRated = ({ pageParam = 1 }) =>
+export const getTopRated = ({ pageParam }) =>
   fetch(
     `${BASE_URL}/top_rated?api_key=${API_KEY}&language=en-US&page=${pageParam}`
   ).then((res) => res.json());
 
-export const getUpcoming = ({ pageParam = 1 }) =>
+export const getUpcoming = ({ pageParam }) =>
   fetch(
     `${BASE_URL}/upcoming?api_key=${API_KEY}&language=en-US&page=${pageParam}`
   ).then((res) => res.json());
 
-export const getDetail = (params) => {
-  const [, movieId] = params.queryKey;
+// Detail.jsx
+export const getDetail = ({ queryKey }) => {
+  const [, movieId] = queryKey;
   return fetch(
     `${BASE_URL}/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=videos`
   ).then((res) => res.json());
 };
+
+// Review.jsx
